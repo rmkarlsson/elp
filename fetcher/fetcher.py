c9d5a37b48a26a8c16233e7156768aedf50daba8
Mikael Karlsson <rmkarlsson@protonmail.ch>
2024-12-26 00:44:24 +0100

    [task] Fetches data correct

--- fetcher/fetcher.py
+++ fetcher/fetcher.py
@@ -1,8 +1,24 @@
 import requests
 
+API_ENDPOINT = "/api/price/spot/pricearea/"
+PRICE_AREA_TAG = "SN"
+PRICE_AREA = "3"
+
+def get_price_blob(price_source, start_date, end_date):
+    url = get_price_url(price_source, start_date, end_date)
+    return fetch_json(url)
+
+
+def get_price_url(price_source, start_date, end_date):
+    return f"{price_source}{API_ENDPOINT}{start_date}/{end_date}/{PRICE_AREA_TAG}{PRICE_AREA}"
+
+
 def fetch_json(url):
     try:
-        response = requests.get(url)
+        headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
+}
+        response = requests.get(url, headers=headers)
         response.raise_for_status()  # Raise an exception for HTTP errors
         return response.json()  # Parse JSON response
     except requests.exceptions.RequestException as e:
--- main.py
+++ main.py
@@ -1,14 +1,16 @@
 import argparse
-from fetcher.fetcher import fetch_json
+from fetcher.fetcher import get_price_blob
 
 def main():
     parser = argparse.ArgumentParser(description="Process a string argument.")
-    parser.add_argument('--url', type=str, required=True, help='Energy price source')
+    parser.add_argument('--price_source', type=str, required=False, default='https://www.vattenfall.se', help='Energy price source')
+    parser.add_argument('--start-date', type=str, required=True, help='Start date')
+    parser.add_argument('--end-date', type=str, required=True, help='End date')
     
     args = parser.parse_args()
-    print(f'Eneregy price source: {args.url}')
+    print(f'Eneregy price source: {args.price_source}')
 
-    resp = fetch_json(args.url)
+    resp =  get_price_blob(args.price_source, args.start_date, args.end_date)
     print(resp)
 
 if __name__ == "__main__":
